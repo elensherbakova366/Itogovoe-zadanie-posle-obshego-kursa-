ae60e3431f1486449fa5112dabbb277d2b8c7aa2
ElenSherbakova366@yandex.ru <ElenSherbakova366Yandex.ru>
2022-09-17 11:27:50 +0400

    Dobavila rashirenie

---
+++ Itogovaya zadacha.cs
@@ -0,0 +1,17 @@
+; string [] array = new string [] {"hello","2","world",":-)"};
+; Console.WriteLine($"{"Было:"} {"hello"},{"2"},{"world"},{":-)"}");
+; Console.WriteLine("Cтало: " + array[1] + "," + array[3]);
+
+string [] strArray = new string [] {"hello","2","world",":-)"};
+string a = "hello";
+string b = "2";
+string c = "world";
+string d = ":-)";
+Console.WriteLine("Длины строк: " + $"{a.Length}, {b.Length}, {c.Length}, {d.Length}");
+Console.WriteLine("Строки,у которых длина <=3: ");
+    if (a.Length<=3) Console.WriteLine(a);
+    if (b.Length<=3) Console.WriteLine(b);
+    if (c.Length<=3) Console.WriteLine(c);
+    if (d.Length<=3) Console.WriteLine(d);
+Console.WriteLine("Массив строк с длиной <=3: ");
+Console.WriteLine($"{b}{","}{d}");
